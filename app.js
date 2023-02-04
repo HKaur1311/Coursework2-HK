f196bc9dfef6f7d429f39d612ff0bf4b31281a63
HK <hk489227@gmail.com>
2023-02-04 23:44:09 +1100

    Changed Vue Functionality To support XMLRequests
    Changed Vue Functionality To support XMLRequests

--- app.js
+++ app.js
@@ -7,10 +7,38 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+const uri = "mongodb://localhost:27017/test";
+
+MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const db = client.db('afterschool');
+  app.get('/lessons', (req, res) => {
+    db.collection('courses').find({}).toArray((err, courses) => {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      res.json(courses);
+    });
+  });
+  app.get('/user', (req, res) => {
+    db.collection('user').find({}).toArray((err, user) => {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      res.json(user);
+    });
+  });
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 
   app.listen(3000, () => {
     console.log("Server started on http://localhost:3000");
   });
+});
