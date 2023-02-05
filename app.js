4555cd28e2713ecf99326f8a479086cb5213d3db
HK <hk489227@gmail.com>
2023-02-05 13:28:31 +1100

    Cleared Clutter
    Cleared Clutter

--- app.js
+++ app.js
@@ -117,9 +117,6 @@ app.put("/lessons/:id", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 //start server
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
